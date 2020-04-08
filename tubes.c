b4e885a38372ac90d9eb013800f8aaae0a77b213
sandrayoe <60214652+sandrayoe@users.noreply.github.com>
2020-04-08 11:45:16 +0700

    Update tubes.c

--- tubes.c
+++ tubes.c
@@ -5,16 +5,24 @@ int cols;
 int rows; 
 
 //ini fungsi untuk menghitung tetangga sel (Neighbor) 
-int Neighbors (int cell[cols][rows],int x, int y) {
-	int i,j,sum; 
+//x dan y adalah indeks lokasi saat itu
+int Neighbors (char cell[cols][rows],int x, int y) {
+	int i,j;
+	int sum = 0;
 	for (i = -1; i<2; i++) { 
 		for (j = -1; j<2; j++) { 
-			sum = sum + cell[x+i][y+j];
+			if (cell[x+i][y+j] == 'X') { 
+				sum = sum + cell[x+i][y+j];
+			}
 		}
 	}
-	sum = sum - cell[x][y]; //karena tidak menghitung dirinya sendiri
-	return sum; 
+	if (cell[x][y] == 'X') { 
+		sum = sum - cell[x][y]; //karena tidak menghitung dirinya sendiri
+	}
+	return sum;
 } 
+
+
 
 
 

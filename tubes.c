8e7069e7c4aab0d15423bfab60d12d71481260ae
sandrayoe <60214652+sandrayoe@users.noreply.github.com>
2020-04-08 11:20:18 +0700

    Update tubes.c

--- tubes.c
+++ tubes.c
@@ -1,6 +1,20 @@
 //ini buat tubes ppmc
 #include <stdio.h> 
 
+int cols;
+int rows; 
 
-//ini fungsi untuk menghitung tetangga sel (CountNeighbor) 
-int CountNeighbors (
+//ini fungsi untuk menghitung tetangga sel (Neighbor) 
+int Neighbors (int cell[cols][rows],int x, int y) {
+	int i,j,sum; 
+	for (i = -1; i<2; i++) { 
+		for (j = -1; j<2; j++) { 
+			sum = sum + cell[x+i][y+j];
+		}
+	}
+	sum = sum - cell[x][y]; //karena tidak menghitung dirinya sendiri
+	return sum; 
+} 
+
+
+
